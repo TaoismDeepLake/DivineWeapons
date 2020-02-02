48313ea831a601407cd072a589518a80fdcf5cbe
Taoism DeepLake <pymess@icloud.com>
2020-02-02 16:07:39 +0800

    Revert "manual WIP"
    This reverts commit b2e633d394a14009b746dd51b7f25f51407a0847.

--- src/main/java/com/deeplake/dweapon/init/events/ModStarterEvents.java
+++ src/main/java/com/deeplake/dweapon/init/events/ModStarterEvents.java
@@ -91,7 +91,7 @@ public class ModStarterEvents {
 
 			String pageCountString = I18n.format(name + DWNBTDef.MANUAL_PAGE_COUNT);
 			
-			int pageCount = 10;
+			int pageCount = 0;
 			boolean hasManual = false;
 			try {
 				pageCount = Integer.parseInt(pageCountString);
